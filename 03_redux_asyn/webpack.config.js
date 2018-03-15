61bec80febcfaa4314acc5776c587003adecf06b
Leron <leron1989@outlook.com>
2018-03-15 15:46:14 +0800

    redux asyn

---
+++ 03_redux_asyn/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+    entry:{
+        app:'./src/index.js'
+    },
+    output:{
+        filename:'[name].bundle.js',
+        path:path.resolve(__dirname, 'dist')
+    },
+    devtool: 'inline-source-map',
+    devServer:{
+        contentBase: './dist'
+    },
+    module:{
+        rules:[
+            {
+                test:/\.js$/,
+                exclude:/node_modules/,
+                loaders:'babel-loader',
+                query:{
+                    presets:['es2015','react','stage-0']
+                }
+            }
+        ]
+    }
+}
